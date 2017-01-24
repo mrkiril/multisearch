c0714d47a5ae8b9c7fa1d1331dae06da35d709c4
Kyrylo Kukhelnyi <mrkiril@ukr.net>
2017-01-24 16:38:12 +0200

    add setting file

--- tests/test_multi.py
+++ tests/test_multi.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import datetime
 import time
 import re
@@ -15,10 +16,12 @@ import hashlib
 import socket
 import unittest
 from httpclient import HttpClient
-
+import subprocess
+import multiprocessing
+from time import sleep
+import signal
 
 class Test_serv(unittest.TestCase):
-
     def setUp(self):
         self.file_path = os.path.abspath(os.path.dirname(__file__))
         my_headers = [('User-Agent', 'Mozilla/4.0'), ('X-From', 'UA')]
@@ -34,69 +37,159 @@ class Test_serv(unittest.TestCase):
             max_redirects=10,           # follow Location: header on 3xx response
             set_referer=True,           # set Referer: header when follow location
             keep_alive=3,               # Keep-alive socket up to N requests
-            # headers=my_headers,         # send custom headers
+            #headers=my_headers,         # send custom headers
             http_version="1.1",         # use custom http/version
-            # auth=my_user_pass,          # http auth
+            #auth=my_user_pass,          # http auth
             retry=5,                    # try again on socket or http/5xx errors
             retry_delay=10)             # wait betweet tries
 
+        os.chdir("../")        
+        
+        p = multiprocessing.Process(target=self.process, daemon=False)
+        p.start()
+        self.pid = p.pid
+        sleep(1)
+
+    def process(self):
+        subprocess.call(["python3","search_serv.py"])
+
+    def tearDown(self):
+        os.kill(self.pid, signal.SIGKILL)
+
+        print("slave >> "+str(self.pid))
+        print("head >> "+str(os.getpid()))
+        _=input("Br point")
+    
+    
     def test_page(self):
-
+        #_=input()
         res = self.client.get('http://127.0.0.1:8080/search?q=tarantino')
-        pat1 = re.search("tarantino", res.body)
+        pat1 = re.search("tarantino", res.body)        
         # перевірка на успішність запиту
         self.assertEqual(res.status_code, "200")
         # перевірка на наявність слова в видачі
-        self.assertIsNotNone(pat1)
-
+        self.assertIsNotNone(pat1) 
+        
+        
         res = self.client.get('http://127.0.0.1:8080/search?q=Рогнар+Лодброк')
-        pat1 = re.search("Рогнар", res.body)
-        pat2 = re.search("Лодброк", res.body)
+        pat1 = re.search("Рогнар", res.body) 
+        pat2 = re.search("Лодброк", res.body)        
         # перевірка на успішність запиту
         self.assertEqual(res.status_code, "200")
         # перевірка на наявність слова в видачі
-        self.assertIsNotNone(pat1)
+        self.assertIsNotNone(pat1) 
         self.assertIsNotNone(pat2)
-
+        
+    '''    
+    
     def test_wrong_link(self):
-        res = self.client.get(
-            'http://127.0.0.1:8080/wrong_page.,!@#$%^&*(WTF_page)')
+        res = self.client.get('http://127.0.0.1:8080/wrong_page.,!@#$%^&*(WTF_page)')
         # перевірка на успішність запиту
         self.assertEqual(res.status_code, "404")
-
+    
+    
+       
     def test_wrong_req(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         addr = ("127.0.0.1", 8080)
         sock.connect(addr)
         CRLF = b"\r\n"
-        q = b"GETT /search?q=tarantino HTTP/1.1" + CRLF
-        q += b"User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)" + CRLF
-        q += b"Host: 127.0.0.1:8080" + CRLF
-        q += b"Connection: Close" + CRLF
+        q =  b"GETT /search?q=tarantino HTTP/1.1"+CRLF
+        q += b"User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)"+CRLF
+        q += b"Host: 127.0.0.1:8080"+CRLF
+        q += b"Connection: Close"+CRLF
         q += CRLF
         sock.send(q)
-        response = b""
+        response = b""        
         response += sock.recv(65535)
         status = re.search(b"HTTP.*? (\d+) ", response[:16])
         status_code = status.group(1).decode()
         self.assertEqual(status_code, "400")
+    
 
     def test_wrong_req_2(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         addr = ("127.0.0.1", 8080)
         sock.connect(addr)
         CRLF = b"\r\n"
-        q = b"/GET /search?q=tarantino HTTP/1.1" + CRLF
-        q += b"User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)" + CRLF
-        q += b"Host: 127.0.0.1:8080" + CRLF
-        q += b"Connection: Close" + CRLF
+        q =  b"/GET /search?q=tarantino HTTP/1.1"+CRLF
+        q += b"User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)"+CRLF
+        q += b"Host: 127.0.0.1:8080"+CRLF
+        q += b"Connection: Close"+CRLF
         q += CRLF
         sock.send(q)
-        response = b""
+        response = b""        
         response += sock.recv(65535)
         status = re.search(b"HTTP.*? (\d+) ", response[:16])
         status_code = status.group(1).decode()
         self.assertEqual(status_code, "400")
+    
+
+    '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
