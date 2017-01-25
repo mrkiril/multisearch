cff17bd5f7fb03ee08b962c8b7a7d2c3b68c8a78
Kyrylo Kukhelnyi <mrkiril@ukr.net>
2017-01-25 02:06:38 +0200

    hope last 2

--- search_resp.py
+++ search_resp.py
@@ -294,7 +294,7 @@ class ResultsMerger:
             arr_status = [ob.isready() for ob in arr_obj]
             if False in arr_status:
                 sleep(0.05)
-                if time.time() - global_start_time > 1.5:
+                if time.time() - global_start_time > 3.5:
                     break                
                 if time.time() - global_start_time > 0.9:
                     count = arr_status.count(True)
